8f1cfa70e3ffac8580f7c27ca0e7a53f12b8831c
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-04-01 13:30:59 +0200

    switch to tol mode

--- solvers/scipy-linprog.py
+++ solvers/scipy-linprog.py
@@ -1,12 +1,15 @@
+import warnings
 from benchopt import BaseSolver, safe_import_context
 
 
 with safe_import_context() as import_ctx:
     import numpy as np
     from scipy.optimize import linprog
+    from scipy.optimize import OptimizeWarning
+    from scipy.linalg import LinAlgWarning
 
 
-def quantile_regression(X, y, quantile, reg, n_iter, solver, fit_intercept=True):
+def quantile_regression(X, y, quantile, reg, tol, solver, fit_intercept=True):
     n_samples, n_features = X.shape
     sample_weights = np.ones(n_samples) / n_samples
     n_params = n_features
@@ -47,12 +50,20 @@ def quantile_regression(X, y, quantile, reg, n_iter, solver, fit_intercept=True)
 
     b_eq = y
 
+    if 'highs' in solver:
+        options = {'primal_feasibility_tolerance': tol}
+    else:
+        options = {'tol': tol}
+
+    warnings.filterwarnings('ignore', category=OptimizeWarning)
+    warnings.filterwarnings('ignore', category=LinAlgWarning)
+
     result = linprog(
         c=c,
         A_eq=A_eq,
         b_eq=b_eq,
         method=solver,
-        options={'maxiter': n_iter},
+        options=options,
     )
     solution = result.x
     params = solution[:n_params] - solution[n_params:2 * n_params]
@@ -82,13 +93,15 @@ class Solver(BaseSolver):
             'highs', 'highs-ipm', 'highs-ds'
         ],
     }
+    stop_strategy = 'tolerance'
 
     def set_objective(self, X, y, reg, quantile):
         self.X, self.y, self.reg, self.quantile = X, y, reg, quantile
 
-    def run(self, n_iter):
+    def run(self, tol):
+        tol = 1e-3 * tol
         self.coef_, self.intercept_ = quantile_regression(
-            self.X, self.y, self.quantile, self.reg, n_iter,
+            self.X, self.y, self.quantile, self.reg, tol,
             self.solver, fit_intercept=True
         )
 
