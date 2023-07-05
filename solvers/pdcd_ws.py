b760a4938cee2d52e3306ee7c00b2b4704567943
mathurinm <mathurin.massias@gmail.com>
2023-07-05 10:30:09 +0200

    install from pip

--- solvers/pdcd_ws.py
+++ solvers/pdcd_ws.py
@@ -13,7 +13,7 @@ class Solver(BaseSolver):
     name = "PDCD-WS"
 
     install_cmd = 'conda'
-    requirements = ["skglm>=0.3"]
+    requirements = ["pip:skglm>=0.3"]
 
     references = [
         'Q. Bertrand and Q. Klopfenstein and P.-A. Bannier and G. Gidel '
